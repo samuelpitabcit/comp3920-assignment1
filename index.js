3687d25db3fdd19722e5c75e7b4d2fcf5edc4c37
samuelpitabcit <spita@my.bcit.ca>
2026-02-02 16:03:32 -0800

    Completed login and logout of index.js

--- index.js
+++ index.js
@@ -1,15 +1,200 @@
+require("dotenv").config();
+
 const express = require("express");
+const bcrypt = require("bcrypt");
+const mysql = require("mysql2/promise");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 const path = require("path");
+
 const app = express();
+
 const PORT = process.env.PORT || 3000;
+const SALT_ROUNDS = 12;
+
+//
+// Set up connection to MySQL
+//
+
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+});
+
+//
+// Set up session cookies middleware
+//
+
+const store = new MongoDBStore({
+    uri: process.env.MONGODB_URI,
+    collection: "assignment1_sessions",
+});
+
+store.on("error", (error) => console.log(error));
+
+app.use(
+    session({
+        secret: process.env.MONGODB_SESSION_SECRET,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24 * 7,
+            secure: process.env.NODE_ENV === "prod",
+        },
+        store: store,
+        resave: false,
+        saveUninitialized: true,
+    }),
+);
+
+//
+// Set up EJS and POST middleware.
+//
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "routes"));
-
 app.use(express.static(path.join(__dirname, "static")));
+
+//
+// Functions
+//
+
+/**
+ * Checks the given password's strength.
+ * @param {string} password
+ * @returns {bool}
+ */
+function isPasswordStrong(password) {
+    const MIN_LENGTH = 8;
+
+    if (password < MIN_LENGTH) {
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * Filters clients that are not logged in.
+ *
+ * @param {Request} req
+ * @param {Response} res
+ * @param {import("express").NextFunction} next
+ */
+function filterNotLoggedIn(req, res, next) {
+    if (req.session.userId == null) {
+        res.status(307).redirect("/login");
+        return;
+    }
+
+    next();
+}
+
+/**
+ * Filters clients already logged in.
+ *
+ * @param {Request} req
+ * @param {Response} res
+ * @param {import("express").NextFunction} next
+ */
+function filterLoggedIn(req, res, next) {
+    if (req.session.userId != null) {
+        res.status(301).redirect("/account");
+        return;
+    }
+
+    next();
+}
+
+//
+// API Routes
+//
+
+app.post("/api/login", filterLoggedIn, async (req, res) => {
+    /**
+     * @typedef LoginBody
+     * @property {string} username
+     * @property {string} password
+     */
+
+    /** @type {LoginBody} */
+    const login = req.body;
+
+    const [rows] = await pool
+        .query("SELECT user_id, passwd FROM user WHERE username = ?", [login.username])
+        .catch((reason) => {
+            res.status(500).send(reason);
+        });
+
+    for (const { user_id, passwd } of rows) {
+        if (bcrypt.compareSync(login.password, passwd)) {
+            req.session.userId = user_id;
+            res.status(301).redirect("/account");
+            return;
+        }
+    }
+
+    res.status(401).render("login", { message: "Username and password do not match!" });
+});
+
+app.get("/api/logout", (req, res) => {
+    req.session.destroy((err) => {
+        if (err) console.log(err);
+        res.status(301).redirect("/");
+    });
+});
+
+app.post("/api/register", filterLoggedIn, async (req, res) => {
+    /**
+     * @typedef RegisterBody
+     * @property {string} username
+     * @property {string} password
+     */
+
+    /** @type {RegisterBody} */
+    const { username, password } = req.body;
+
+    if (!isPasswordStrong(password)) {
+        res.status(401).send("Invalid password!");
+        return;
+    }
+
+    await bcrypt
+        .hash(password, SALT_ROUNDS)
+        .then((passwordHash) =>
+            pool.query("INSERT INTO user (username, passwd) VALUES (?, ?)", [
+                username,
+                passwordHash,
+            ]),
+        )
+        .then(([results]) => {
+            req.session.userId = results.insertId;
+            req.status(301).redirect("/account");
+        })
+        .catch((reason) => res.status(401).send("Failed to create an account | " + reason));
+});
+
+//
+// Page Routes
+//
 
 app.get("/", (_, res) => {
     res.render("index");
+});
+
+app.get("/login", filterLoggedIn, (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", filterLoggedIn, (req, res) => {
+    res.render("register");
+});
+
+app.get("/account", filterNotLoggedIn, (req, res) => {
+    res.send("Account page.");
 });
 
 app.use((_, res) => {
